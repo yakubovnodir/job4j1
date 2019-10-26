4b9ea87ad567e0ae44b6392de028a0dc319ba2eb
yakubovnodir <yanodir@gmail.com>
2019-10-26 19:44:48 +0500

    Макс число

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,8 @@
+package main.java.ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left>right?left:right;
+        return result;
+    }
+}
