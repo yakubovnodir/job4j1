cb2ff595d508362385071640cb767ad82bcfeed4
yakubovnodir <yanodir@gmail.com>
2019-11-09 09:28:43 +0500

    MultiMax2

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -5,7 +5,21 @@ public class MultiMax {
 
 
 
-        int result = (first>second && first>third)?first:(second>first && second>third)?second:(third>first && third>second)?third:third;
+        int result = 0;//(first>second && first>third)?first:(second>first && second>third)?second:(third>first && third>second)?third:third;
+
+        if (first>second && first>third)
+        {
+            result=first;
+        }
+        else if (second>first && second>third)
+        {
+            result=second;
+        }
+        else if (third>first && third>second)
+        {
+            result=third;
+        }
+
         return result;
     }
 }
--- chapter_001/src/main/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMaxTest.java
@@ -10,4 +10,16 @@ public class MultiMaxTest {
         int result = check.max(2, 1, 4);
         assertThat(result, is(4));
     }
+    @Test
+    public void whenSecondMax2() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 1, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenSecondMax3() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
 }
