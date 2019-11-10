ed65cc9d0847f972edcc5dae1cc41d5c4961b758
yakubovnodir <yanodir@gmail.com>
2019-11-10 14:13:44 +0500

    Counter_vertion_2

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -3,15 +3,7 @@ package ru.job4j.loop;
 public class Counter {
     public int add(int start, int finish) {
         int sum = 0;
-
-        for (int i=start;i<=finish;i++)
-        {
-            if (i%2==0)
-            {
-                sum=sum+i;
-            }
-        }
-
+        for (int i=start;i<=finish;i++) if (i%2==0) sum=sum+i;
         return sum;
     }
 }
