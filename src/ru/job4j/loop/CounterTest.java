99f0ad2f8466d066505f7d945857d5716214af08
yakubovnodir <yanodir@gmail.com>
2019-11-10 11:54:24 +0500

    Counter

---
+++ src/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter check = new Counter();
+        int result = check.add(0,10);
+        assertThat(result, is(30));
+        //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
+    }
+}
