c555268174df1e07d88ae38c21644e2deddf7021
yakubovnodir <yanodir@gmail.com>
2019-11-10 09:59:57 +0500

    SqMax_tests

--- chapter_001/src/main/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMaxTest.java
@@ -6,10 +6,38 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
+   // Добавьте еще три теста на выборки (5,4, 2, 1) (1,5, 2, 3) (1,4, 5, 2) соответственно
+
+
     @Test
     public void whenSqMax() {
         SqMax check = new SqMax();
         int result = check.max(1,4, 2, 5);
         assertThat(result, is(5));
+        //
+    }
+
+    @Test
+    public void whenSqMax1() {
+        SqMax check = new SqMax();
+        int result = check.max(5,4, 2, 1);
+        assertThat(result, is(5));
+        //
+    }
+
+    @Test
+    public void whenSqMax2() {
+        SqMax check = new SqMax();
+        int result = check.max(1,5, 2, 3);
+        assertThat(result, is(5));
+        //
+    }
+
+    @Test
+    public void whenSqMax3() {
+        SqMax check = new SqMax();
+        int result = check.max(1,4, 5, 2);
+        assertThat(result, is(5));
+        //
     }
 }
