98d751b22919751d510a346b0ca448e74095ad74
yakubovnodir <yanodir@gmail.com>
2020-01-05 14:51:44 +0500

    JavaDoc

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,14 @@
+package main.java.ru.job4j.calculate;
+/**
+ * Package for calculate task.
+ *
+ * @author Nodir Yakubov (yanodir@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package_info.java
@@ -0,0 +1,10 @@
+package main.java.ru.job4j.calculate;
+/**
+ * Package for calculate task.
+ *
+ * @author Nodir Yakubov (yanodir@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class package_info {
+}
