db308fb08e3d1607a018bb035c119f7a430ab606
yakubovnodir <yanodir@gmail.com>
2020-01-12 12:15:31 +0500

    fix5

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,7 +7,10 @@ package main.java.ru.job4j.calculate;
  * @since 0.1
  */
 public class Calculate {
-
+    /**
+     * Main.
+     * @param args
+     */
     public static void main(String[] args)
     {
 
