d9ef890077ab275dedb22f15951c238ae21d642b
yakubovnodir <yanodir@gmail.com>
2020-01-12 12:33:50 +0500

    Slah_X_figure_complete_method_draw

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,31 @@
+package main.java.ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row==cell?true:false; // добавить условие, по которому нужно определить ставить ли символ или нет.
+                boolean right = cell==size-row-1?true:false; // добавить условие, что нужно ставить элемент в правый угол.
+
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+        System.out.println("Draw by 9");
+        draw(9);
+    }
+}
