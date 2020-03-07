f37fbeeb3584731a25c07386e6bf35648da4c8d6
yakubovnodir <yanodir@gmail.com>
2020-03-07 07:07:27 +0500

    Check_with_test2

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,16 +3,21 @@ package main.java.ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = false;
-        int same_element_values_count=0;
 
         result=data.length==0;
         if (result) return result;
-        else
-        for (int i=0;i<data.length;i++)
-        same_element_values_count=same_element_values_count+(data[i]?1:0);
+        else {
 
-        return (same_element_values_count==data.length);
-
-
+            for (int i = 0; i < data.length; i++)
+            {
+                if (data[0]!=data[i])
+                {
+                    result=false;
+                    return result;
+                }
+            }
+            result=true;
+        }
+        return (result);
     }
 }
