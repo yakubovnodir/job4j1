4a3e4b916420cd0f08a490fc7e285de4f28f8c40
yakubovnodir <yanodir@gmail.com>
2020-03-15 07:38:53 +0500

    ArrayChar (code correct)

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -7,11 +7,8 @@ public class Check {
         result=data.length==0;
         if (result) return result;
         else {
-
-            for (int i = 0; i < data.length; i++)
-            {
-                if (data[0]!=data[i])
-                {
+            for (int i = 0; i < data.length; i++) {
+                if (data[0]!=data[i]) {
                     result=false;
                     return result;
                 }
