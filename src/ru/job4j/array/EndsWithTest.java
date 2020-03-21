726e1147ef0af3da87218af6c584a6f9a24afcbc
yakubovnodir <yanodir@gmail.com>
2020-03-21 11:31:38 +0500

    EndsWith_edited

--- src/ru/job4j/array/EndsWithTest.java
+++ src/ru/job4j/array/EndsWithTest.java
@@ -26,7 +26,7 @@ public class EndsWithTest {
     @Test
     public void whenEndWithPrefixThenTrue2() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] post = {'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result, is(true));
     }
