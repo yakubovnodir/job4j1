d195e00bd4a9b1611bdfc0086baab8fd6513dcb5
yakubovnodir <yanodir@gmail.com>
2020-06-13 10:54:47 +0500

    MaxrixCheck_fixes_2

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -3,40 +3,41 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        result=monoHorizontal(board);
-        result=monoVertical(board);
-
-        return result;
-    }
-    private static boolean monoHorizontal(char[][] board){
-        boolean result = false;
-        for (int i=0;i<board.length;i++) {
-            int check=0;
-            for (int j=0;j<board.length;j++) {
-                if (board[i][j] =='X') {
-                    check++;
-                }
+        for (int i = 0; i < board.length; i++) {
+            result = monoHorizontal(board, i);
+            if (result){
+                return result;
             }
-            if (check==board.length) {
-                result = true;
-                break;
+            result = monoVertical(board, i);
+            if (result){
+                return result;
             }
         }
         return result;
     }
-    private static boolean monoVertical(char[][] board){
+    private static boolean monoHorizontal(char[][] board, int i){
         boolean result = false;
-        for (int i=0;i<board.length;i++) {
-            int check=0;
-            for (int j=0;j<board.length;j++) {
-                if (board[j][i] =='X') {
-                    check++;
-                }
+        int check = 0;
+        for (int j = 0; j < board.length; j++) {
+            if (board[i][j] == 'X') {
+                check++;
             }
-            if (check==board.length) {
-                result = true;
-                break;
+        }
+        if (check == board.length) {
+            result = true;
+        }
+        return result;
+    }
+    private static boolean monoVertical(char[][] board, int j){
+        boolean result = false;
+        int check = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][j] =='X') {
+                check++;
             }
+        }
+        if (check==board.length) {
+            result = true;
         }
         return result;
     }
